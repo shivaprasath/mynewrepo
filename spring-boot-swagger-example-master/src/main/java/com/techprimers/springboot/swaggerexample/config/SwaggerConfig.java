b05281fdde157c82f221c2efa436fd00d8681a1a
shivaprasath <shivaprasath.software@gmail.com>
2018-10-02 15:59:02 -0400

    test app

---
+++ spring-boot-swagger-example-master/src/main/java/com/techprimers/springboot/swaggerexample/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.techprimers.springboot.swaggerexample.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.techprimers.springboot.swaggerexample"))
+                .paths(regex("/rest.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "API Developer Center",
+                "Api Developer center",
+                "1.0",
+                "Terms of Service",
+                new Contact("Shivaprasath", "https://www.testsight.com/shivtech",
+                        "shivaprasath.software@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html"
+        );
+
+        return apiInfo;
+    }
+}
